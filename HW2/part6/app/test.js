f993350fc7fd1cd51b88d5d6a51b034d27e8a389
Artem Zinchenko <lexart666@gmail.com>
2023-03-13 00:47:21 +0200

    update task

---
+++ HW2/part6/app/test.js
@@ -0,0 +1,61 @@
+// let currentDate = new Date();
+// if (currentDate.getHours() >= 5 && currentDate.getHours() < 11) {
+//     alert("Доброї ночі");
+// } if (currentDate.getHours() >= 11 && currentDate.getHours() < 17) {
+//     alert("Доброго дня");
+// } if (currentDate.getHours() >= 17 && currentDate.getHours() < 23) {
+//     alert("Доброго вечора");
+// } alert("Доброї ночі");
+
+let currentDate = new Date();
+switch (currentDate.getHours()) {
+    case 23: alert("Доброї ночі");
+        break;
+    case 0: alert("Доброї ночі");
+        break;
+    case 1: alert("Доброї ночі");
+        break;
+    case 2: alert("Доброї ночі");
+        break;
+    case 3: alert("Доброї ночі");
+        break;
+    case 4: alert("Доброї ночі");
+        break;
+    case 5: alert("Доброї ночі");
+        break;
+    case 6: alert("Доброго ранку");
+        break;
+    case 7: alert("Доброго ранку");
+        break;
+    case 8: alert("Доброго ранку");
+        break;
+    case 9: alert("Доброго ранку");
+        break;
+    case 10: alert("Доброго ранку");
+        break;
+    case 11: alert("Доброго дня");
+        break;
+    case 12: alert("Доброго дня");
+        break;
+    case 13: alert("Доброго дня");
+        break;
+    case 14: alert("Доброго дня");
+        break;
+    case 15: alert("Доброго дня");
+        break;
+    case 16: alert("Доброго дня");
+        break;
+    case 17: alert("Доброго вечора");
+        break;
+    case 18: alert("Доброго вечора");
+        break;
+    case 19: alert("Доброго вечора");
+        break;
+    case 20: alert("Доброго вечора");
+        break;
+    case 21: alert("Доброго вечора");
+        break;
+    case 22: alert("Доброго вечора");
+        break;
+}
+
