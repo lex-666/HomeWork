847cffcf8f8dc5899cb18c9a978395758c0acc63
Artem Zinchenko <lexart666@gmail.com>
2023-03-13 17:15:28 +0200

    update part4

--- HW2/part4/app/test.js
+++ HW2/part4/app/test.js
@@ -1,25 +1,19 @@
 let arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5]
-let data = {}; // 5
+let data = {};
 let count = 0;
 let newArr = [];
 for (const item of arr) {
     data[item] = data[item] ? data[item] + 1 : 1;     
 }
 const result = Object.keys(data).filter((item) => data[item] > 1);
-//console.log(result); 
-
 for (const i of result) {
   if (i > count) {
     count = i;
   }
   }
-  let myArray = ['one', 'two', 'three'];
-let newArray = myArray.filter(function(f) { return f !== 'two' });
-
-console.log(newArray)
-// for (const y of arr) {
-//   if (y !== Number(count)) {  
-//     arr.push(y);
-//   }
-// }
-// console.log(arr);
+for (const y of arr) {
+  if (y !== Number(count)) {  
+    newArr.push(y);
+  }
+}
+console.log(newArr);
