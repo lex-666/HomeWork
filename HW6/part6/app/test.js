f1d97f133b7fd09f899a93f1df58b75ce4ba085e
Artem Zinchenko <lexart666@gmail.com>
2023-04-11 13:45:19 +0300

    update

--- HW6/part6/app/test.js
+++ HW6/part6/app/test.js
@@ -2,6 +2,7 @@
  //ціну товару).
 // Придумати ще 2 варіації, відяких буде залежати ціна товару.
 const colors = document.querySelectorAll(".color");
+const sizes = document.querySelectorAll(".size");
 
 colors.forEach((color) => {
   color.addEventListener("click", () => {   
@@ -9,6 +10,33 @@ colors.forEach((color) => {
     document.getElementById("outprice").innerHTML = price;    
     colors.forEach((c) => c.classList.remove("active"));    
     color.classList.add("active");
+  });
+});
+
+sizes.forEach((size) => {
+  size.addEventListener("click", () => {   
+    const price = size.getAttribute("data-price");    
+    document.getElementById("outprice").innerHTML = price;    
+    sizes.forEach((c) => c.classList.remove("active"));    
+    size.classList.add("active");
+  });
+});
+
+
+const colorSpans = document.querySelectorAll('.color');
+colorSpans.forEach(colorSpan => {
+  colorSpan.addEventListener('click', () => {
+    colorSpans.forEach(span => span.classList.remove('active'));
+    colorSpan.classList.add('active');
+    const color = colorSpan.getAttribute('color');
+    const shoeImages = document.querySelectorAll('.shoe');
+    shoeImages.forEach(image => {
+      if (image.getAttribute('color') === color) {
+        image.classList.add('show');
+      } else {
+        image.classList.remove('show');
+      }
+    });
   });
 });
 
